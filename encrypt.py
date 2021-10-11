cb95571927c303a286c11c3101abd2bebe794e2a
Serg <serrg.y85@gmail.com>
2021-10-11 13:36:36 +0300

    fix

--- encrypt.py
+++ encrypt.py
@@ -2,28 +2,18 @@ from cryptography.fernet import Fernet
 import base64
 import hashlib
 
-# Set key word
-key = base64.urlsafe_b64encode(hashlib.sha256(b'key').digest())
+
+def set_key(string: str):
+    return base64.urlsafe_b64encode(hashlib.sha256(string.encode('utf-8')).digest())
 
 
-def encrypt(data, key):
-    """
-    Given a data (bytes) and key (bytes), it encrypts the data and return it
-    """
-    f = Fernet(key)
-
+def encrypt(data, keyword):
+    f = Fernet(keyword)
     encrypted_data = f.encrypt(data)
 
     return encrypted_data
 
 
-def decrypt(filename, key):
-    """
-    Given a filename (str) and key (bytes), it decrypts the file and return data
-    """
-    f = Fernet(key)
-    with open(filename, "rb") as file:
-        # read the encrypted data
-        encrypted_data = file.read()
-    # decrypt data
-    return f.decrypt(encrypted_data)
+def decrypt(data, keyword):
+    f = Fernet(keyword)
+    return f.decrypt(data)
