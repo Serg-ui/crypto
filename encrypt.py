3004ad50cdde3476b2e8df0a6fd3c871d4fe2b27
Serg <serrg.y85@gmail.com>
2021-10-11 14:25:30 +0300

    fix

--- encrypt.py
+++ encrypt.py
@@ -1,6 +1,8 @@
 from cryptography.fernet import Fernet
 import base64
 import hashlib
+from tkinter.filedialog import askopenfilename
+import webbrowser
 
 
 def set_key(string: str):
@@ -17,3 +19,28 @@ def encrypt(data, keyword):
 def decrypt(data, keyword):
     f = Fernet(keyword)
     return f.decrypt(data)
+
+
+if __name__ == '__main__':
+    command = input("Type '1' to encrypt, or '2' to decrypt: ")
+
+    if command == '1':
+        pass
+
+    elif command == '2':
+        filename = askopenfilename()
+        encrypted_file = open(filename, 'rb')
+        encrypted_data = encrypted_file.read()
+
+        keyword = input('keyword: ')
+        decrypted_data = decrypt(encrypted_data, set_key(keyword))
+        encrypted_file.close()
+
+        decr_file = open('decr', 'wb')
+        decr_file.write(decrypted_data)
+        decr_file.close()
+
+        webbrowser.open(decr_file.name)
+
+    else:
+        print('wrong command')
